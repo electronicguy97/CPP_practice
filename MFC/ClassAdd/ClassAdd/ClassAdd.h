dc9c0f30ed6ad38a500a22cef32c808180079ce5
dykim <kdy@zv-lab.com>
2024-12-04 10:51:40 +0900

    MFC Class add 학습

---
+++ MFC/ClassAdd/ClassAdd/ClassAdd.h
@@ -0,0 +1,32 @@
+﻿
+// ClassAdd.h: PROJECT_NAME 응용 프로그램에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CClassAddApp:
+// 이 클래스의 구현에 대해서는 ClassAdd.cpp을(를) 참조하세요.
+//
+
+class CClassAddApp : public CWinApp
+{
+public:
+	CClassAddApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CClassAddApp theApp;
---
+++ MFC/ClassAdd/ClassAdd/ClassAddDlg.h
@@ -0,0 +1,41 @@
+﻿
+// ClassAddDlg.h: 헤더 파일
+//
+
+#pragma once
+
+#include "CInputDlg.h"
+
+// CClassAddDlg 대화 상자
+class CClassAddDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CClassAddDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+	CInputDlg m_InputDlg;
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CLASSADD_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnClickedButtonInput();
+	afx_msg void OnClickedButtonOutput();
+	afx_msg void OnClickedButtonExit();
+};
