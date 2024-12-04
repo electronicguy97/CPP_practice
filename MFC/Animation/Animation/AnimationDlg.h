06a48b0d60df708a0d4af42b92ca24e3230bc307
dykim <kdy@zv-lab.com>
2024-12-04 10:50:36 +0900

    MFC Animation 학습

---
+++ MFC/Animation/Animation/AnimationDlg.h
@@ -0,0 +1,41 @@
+﻿
+// AnimationDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CAnimationDlg 대화 상자
+class CAnimationDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CAnimationDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ANIMATION_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CAnimateCtrl m_cAnimation;
+	CString m_strFileName;
+	afx_msg void OnClickedButtonPlay();
+	afx_msg void OnClickedButtonStop();
+	afx_msg void OnClickedButtonFile();
+	afx_msg void OnClickedButtonExit();
+};
