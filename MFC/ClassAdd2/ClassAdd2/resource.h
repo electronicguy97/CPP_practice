ed1bfc010981813c5d24653b3d0376f179c8736a
dykim <kdy@zv-lab.com>
2024-12-04 10:51:51 +0900

    MFC Class  학습

---
+++ MFC/ClassAdd2/ClassAdd2/resource.h
@@ -0,0 +1,37 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// ClassAdd2.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CLASSADD2_DIALOG            102
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG_IN_NAME              129
+#define IDD_DIALOG_IN_CONTACT           131
+#define IDD_DIALOG_OUT_ALL              132
+#define IDC_BUTTON_NAME_IN              1006
+#define IDC_BUTTON_CONTACT_IN           1007
+#define IDC_BUTTON_NAME_OUT             1008
+#define IDC_BUTTON_CONTACT_OUT          1009
+#define IDC_BUTTON_ALL_OUT              1009
+#define IDC_BUTTON_EXIT                 1010
+#define IDC_EDIT_IN_KOR_NAME            1011
+#define IDC_EDIT_IN_ENG_NAME            1012
+#define IDC_EDIT_IN_EMAIL               1013
+#define IDC_EDIT_IN_CELL                1014
+#define IDC_EDIT_OUT_KOR_NAME           1015
+#define IDC_EDIT_OUT_ENG_NAME           1016
+#define IDC_EDIT_OUT_EMAIL              1017
+#define IDC_EDIT_OUT_CELL               1018
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1020
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
